c8d9f0f374d85f6966bc91e69b7674e856bfd0a6
andrewjacobkennedy <andrewjacobkennedy@gmail.com>
2021-02-03 16:08:38 -0800

    Update archive_build.py script

--- tools/archive_build.py
+++ tools/archive_build.py
@@ -1,6 +1,10 @@
 import os
+import re
 import sys
 import zipfile
+
+
+version_re = re.compile(r"^__version__ = \"(.*)\"$", re.I)
 
 
 def create_archive(folder_path: str) -> int:
@@ -8,7 +12,9 @@ def create_archive(folder_path: str) -> int:
         print(f"{folder_path} is not a directory.")
         return 1
 
-    zip_file = f"{folder_path}.zip"
+    version = get_version()
+    zip_file = f"{folder_path}_{version}_win.zip"
+
     if os.path.isfile(zip_file):
         print(f"{zip_file} already exists.")
         return 1
@@ -20,6 +26,20 @@ def create_archive(folder_path: str) -> int:
                 rel_path = os.path.relpath(full_path, folder_path)
                 zf.write(full_path, arcname=rel_path)
     return 0
+
+
+def get_version() -> str:
+    tools_dir = os.path.abspath(os.path.dirname(__file__))
+    project_dir = os.path.dirname(tools_dir)
+    version_file = os.path.join(project_dir, "__version__.py")
+
+    with open(version_file, 'r') as fp:
+        line = fp.read().strip()
+
+    match = version_re.match(line)
+    version = match.group(1)
+
+    return version
 
 
 if __name__ == "__main__":
